05c31665452de56b75b4a2dfe63cacd88ea3771a
Bruno da Luz <brunolabx@gmail.com>
2015-05-06 15:17:03 -0300

    abstract data

---
+++ abstractData.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+#by v0id
+
+#just another test with regex
+
+import re
+
+file=open("login.html","r")
+line=file.readline()
+
+tag=re.compile("(<) (html) (>)")
+result=tag.match(line)
+
+html=file.read()
+form=re.search("<form.+",html).group()
+
+start=re.search(" ",form).end()
+end=re.search(">",form).start()
+
+parameters=form[start:end]
+pattern=re.compile('(\w+)="([\w.]+)"')
+
+result=pattern.findall(parameters)
+for element in result:
+ print("\033[37m %s -->\033[36m %s"%(element[0],element[1]))
